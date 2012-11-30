49fe2c306b329c3f0d2c56922c441e0515c860bb
fanzhang312 <fanzhang312@gmail.com>
2012-11-30 11:11:07 -0600

    fetch twitter feed using pythontwitter

---
+++ mysite/feeds/views.py
@@ -0,0 +1,18 @@
+# Create your views here.
+#from django.template import Context, loader
+from django.shortcuts import render_to_response
+from django.http import HttpResponse
+import simplejson
+import httplib2
+import twitter
+import jsonpickle
+
+def index(request):
+    api = twitter.Api(consumer_key='Ad3m2tX4qiwFvCEJw8HOOg',consumer_secret='Nzm3id4JGulXULeGVj2aiPWsxZxFvcXWuosr22D89AI',access_token_key='217970228-oc92QK9dInCRjuNE1dvxgPNuPC9dcvQh7uVPy5vB',access_token_secret='Oiru54uiWoe7GMYz59w0ykCf9IACi2s9zGRtWSWA8')
+    # statuses = api.GetUserTimeline(screen_name="fanzhang312", count=5)
+    statuses = api.GetUserTimeline(screen_name="umnSUA", count=3, include_rts=False, include_entities=True, exclude_replies=True)
+
+    status = jsonpickle.encode(statuses)
+    return HttpResponse(status, mimetype="application/json")
+    
+    # return render_to_response('feeds/index.html',{'latest_feed_list':statuses})
