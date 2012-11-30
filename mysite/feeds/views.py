e2017001fee1255532b10a81c03255fafe8ca4d7
fanzhang312 <fanzhang312@gmail.com>
2012-11-30 11:26:00 -0600

    remove count in GetUserTimeline(), replace with list manipulation

--- mysite/feeds/views.py
+++ mysite/feeds/views.py
@@ -9,10 +9,10 @@ import jsonpickle
 
 def index(request):
     api = twitter.Api(consumer_key='Ad3m2tX4qiwFvCEJw8HOOg',consumer_secret='Nzm3id4JGulXULeGVj2aiPWsxZxFvcXWuosr22D89AI',access_token_key='217970228-oc92QK9dInCRjuNE1dvxgPNuPC9dcvQh7uVPy5vB',access_token_secret='Oiru54uiWoe7GMYz59w0ykCf9IACi2s9zGRtWSWA8')
-    # statuses = api.GetUserTimeline(screen_name="fanzhang312", count=5)
-    statuses = api.GetUserTimeline(screen_name="umnSUA", count=3, include_rts=False, include_entities=True, exclude_replies=True)
+    # api = twitter.Api()
+    statuses = api.GetUserTimeline(screen_name="fanzhang312", include_rts=False, include_entities=True, exclude_replies=True)
 
-    status = jsonpickle.encode(statuses)
+    status = jsonpickle.encode(statuses[:3])
     return HttpResponse(status, mimetype="application/json")
     
     # return render_to_response('feeds/index.html',{'latest_feed_list':statuses})
